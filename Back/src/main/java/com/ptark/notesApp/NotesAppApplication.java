e72c5ade84faf2a17b129edee79f0cc53c48fad9
PetarK <komordzic.sr37.2020@uns.ac.rs>
2023-06-13 16:26:53 +0200

    Initial commit

---
+++ Back/src/main/java/com/ptark/notesApp/NotesAppApplication.java
@@ -0,0 +1,13 @@
+package com.ptark.notesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesAppApplication.class, args);
+	}
+
+}
