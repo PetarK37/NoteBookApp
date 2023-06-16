4767d53363b7ca72762dc3a040008f407f25d429
PetarK <komordzic.sr37.2020@uns.ac.rs>
2023-06-16 18:00:15 +0200

    feat: implemented editing note from front and back side

--- Front/notebook-app/src/Components/ExistingNotesSection/ExistingNotesSection.tsx
+++ Front/notebook-app/src/Components/ExistingNotesSection/ExistingNotesSection.tsx
@@ -37,12 +37,26 @@ function ExistingNotesSection() {
     setNotes(newNotes)
   }
 
+  function updateNote(note: Note){
+    const newNotes = notes?.map(n =>{
+      if(n.uuid === note.uuid){
+        n.content = note.content;
+        n.title = note.title;
+        n.version = note.version;
+        return n;
+      }else{
+        return n;
+      }
+    })
+    setNotes(newNotes)
+  }
+
   return (
     <div className='notes-list gap-12 container flex flex-column justify-content-center'>
       <Button onClick={() => setShowAddDialog(true)} text={'Add new note'} className='btn-full add-note-btn' iconPosition='R' icon={AddIcon()}></Button>
       {(notes?.length !== undefined && notes.length > 0) && 
         notes.map(note => {
-          return <NoteCard note={note} key={note.uuid} onDelete={() => removeNote(note.uuid.toString())}></NoteCard>
+          return <NoteCard note={note} key={note.uuid} onDelete={() => removeNote(note.uuid.toString())} onChange={(note : Note) => updateNote(note)}></NoteCard>
         })
       }
      {!(notes?.length !== undefined && notes.length > 0) && <div className='flex justify-content-center align-items-center no-data-container'>
--- Front/notebook-app/src/Components/NoteCard/NoteCard.tsx
+++ Front/notebook-app/src/Components/NoteCard/NoteCard.tsx
@@ -5,24 +5,25 @@ import '../../App.css'
 import Button from '../Button/Button'
 import { useForm } from "react-hook-form";
 import ConfirmationModal from '../Modal/ConfirmationModal'
-import { deleteNote } from '../../Services/ApiService';
+import { deleteNote, updateNote } from '../../Services/ApiService';
 import { toast } from 'react-toastify';
 
 
 interface NoteCardProps {
     note: Note,
     onDelete?: () => void
+    onChange?:  (note : Note) => void
 }
 
 interface NoteEditCardProps {
     note: Note,
-    onSubmit?: () => void,
+    onSubmit?: (note : Note) => void,
     onCancel?: () => void
 }
 
 type NoteState = 'edit' | 'display' | 'delete'
 
-function NoteCard({ note, onDelete }: NoteCardProps) {
+function NoteCard({ note, onDelete,onChange }: NoteCardProps) {
     const [noteState, setNoteState] = useState<NoteState>('display')
 
     const handleDeleteNote = async () => {
@@ -41,7 +42,12 @@ function NoteCard({ note, onDelete }: NoteCardProps) {
     return (
         <div className='note gap-12 flex flex-column '>
             {(noteState === 'display' || noteState === 'delete') && <NoteCardDisplayContent note={note}></NoteCardDisplayContent>}
-            {(noteState === 'edit') && <NoteCardEditContent note={note} onCancel={() => setNoteState('display')}></NoteCardEditContent>}
+            {(noteState === 'edit') && <NoteCardEditContent note={note} 
+            onSubmit={(note: Note) =>{
+                onChange && onChange(note);
+                setNoteState('display')
+                }} 
+            onCancel={() => setNoteState('display')}></NoteCardEditContent>}
             {(noteState === 'display' || noteState === 'delete') &&
                 <div className='note-action-row flex justify-content-between'>
 
@@ -67,15 +73,32 @@ function NoteCardDisplayContent({ note }: NoteCardProps) {
 
 function NoteCardEditContent({ note, onCancel, onSubmit }: NoteEditCardProps) {
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const { register, handleSubmit, getValues, formState: { errors } } = useForm();
+    
+    const handleUpdateNote = async () => {
+        try {
+            if (onSubmit) {
+                const editedNote = await updateNote (note.version, note.uuid.toString(),
+                    {
+                        title: getValues("title"),
+                        content: getValues("content")
+                    })
+                onSubmit(editedNote as Note)
+            }
+        } catch (error) {
+            if (error instanceof Error) {
+                toast.error(error.message, { toastId: "delete" })
+            }
+        }
+    }
 
     return (
-        <form className='eidt-form gap-12 flex flex-column ' onSubmit={handleSubmit(() => { })}>
+        <form className='eidt-form gap-12 flex flex-column ' onSubmit={handleSubmit(handleUpdateNote)}>
             <header className='card flex align-items-center'>
-                <input className='input' defaultValue={note.title} {...register('title')}></input>
+                <input className={`${errors.title ? 'invalid ' : ''}input`} placeholder={`${errors.title ? "This feild is required!" : 'Enter note title'}`} defaultValue={note.title} {...register('title', { required: true })}></input>
             </header>
-            <article className='card flex justify-content-center'>
-                <textarea className='text-area' defaultValue={note.content} {...register('content')} rows={10}></textarea>
+            <article className='flex justify-content-center'>
+                <textarea placeholder={`${errors.content ? "This feild is required!" : 'Enter note content'}`} className={`${errors.content ? 'invalid ' : ''}text-area`} defaultValue={note.content} {...register('content', { required: true })} rows={10}></textarea>
             </article>
             <div className='note-action-row flex justify-content-between'>
                 <Button btnType='submit' text='Save' className='btn-larger btn-edit'></Button>
